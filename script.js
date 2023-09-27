3ffce104f9d0813b983c0ca9ec8810bd4ec3bb97
Lucas Fischer <lucas.fischer2002@t-online.de>
2023-09-27 19:20:06 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const toggleMenu = () => {
+  const el = document.querySelector(".navbar-menu");
+  el.classList.toggle("is-active");
+};
+
+const skills = ["HTML", "CSS", "JavaScript", "React", "Vue", "Node"];
+
+const skillsContainer = document.querySelector(".skills-container");
+
+skills.forEach((item) => {
+  const skillsItem = document.createElement("span");
+  skillsItem.classList.add("tag");
+  skillsItem.classList.add("is-danger");
+  skillsItem.classList.add("is-large");
+  skillsItem.textContent = item;
+  skillsContainer.appendChild(skillsItem);
+});
